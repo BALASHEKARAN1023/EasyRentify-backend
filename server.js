6470a5c6b5fd4d6dca71b6662009958b3c7af98c
Bala <balashekaran2003@gmail.com>
2024-02-01 15:45:25 +0530

    update

--- server.js
+++ server.js
@@ -20,18 +20,18 @@ app.use('/api/bookings/',require('./routes/bookingRoute.js'));
 
 const path = require('path')
 
-if(process.env.NODE_ENV==='production')
-{
+// if(process.env.NODE_ENV==='production')
+// {
 
-    app.use('/' , express.static('client/build'))
+//     app.use('/' , express.static('client/build'))
 
-    app.get('*' , (req , res)=>{
+//     app.get('*' , (req , res)=>{
 
-          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
+//           res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
 
-    })
+//     })
 
-}
+// }
 
 
 app.get('/',(req,res)=>res.send("Hello"));
