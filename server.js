e3f462b6bf888847fe48be6ccf946d2fe28df907
Bala <balashekaran2003@gmail.com>
2024-02-02 11:32:50 +0530

    cors changed

--- server.js
+++ server.js
@@ -6,12 +6,22 @@ const port=process.env.PORT || 5002;
 const dbConnection=require("./DB")
 
 app.use(express.json());
-// app.use(cors({
-//     origin:["https://easy-rentify-fe-npld-f6nm0ilwn-balashekaran-p-rs-projects.vercel.app"],
-//     methods:["GET","POST"],
-//     credentials:true
-// }))
-app.use(cors());
+app.use(cors({
+    origin: function(origin, callback) {
+        // Check if the origin is allowed
+        if (!origin || origin === "https://easy-rentify-fe-npld-f6nm0ilwn-balashekaran-p-rs-projects.vercel.app") {
+            // Allow the request from this origin
+            callback(null, true);
+        } else {
+            // Deny the request from other origins
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+
+// app.use(cors());
 app.use(cookieParser());
 app.use('/api/cars/',require('./routes/carsRoute.js'));
 app.use('/api/users/',require('./routes/usersRoute.js'));
